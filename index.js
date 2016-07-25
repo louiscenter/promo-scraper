0e8d3461d0d7a0fd91deb0dfa61c0c7ad13bad7e
Louis Center <louis@spideroak-inc.com>
2016-07-25 23:03:51 +0200

    Refactor ✨

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var fs = require('fs')
 var path = require('path')
 
-var scraper = require('nightmare')({ show: false })
+var nightmare = require('nightmare')({ show: true })
 var jsdom = require('jsdom')
 var dateFormat = require('dateformat')
 
 console.log('One moment. Logging in...')
 
-scraper
+nightmare
   .goto('https://twitter.com') // open twitter
   .type('input#signin-email.text-input.email-input', process.argv[2]) // enter username
   .wait(1500)
@@ -23,64 +23,78 @@ scraper
   .wait(1500)
   .scrollTo(999999, 0)
   .wait(1500)
-  .evaluate(function () {
-    console.log('Scraping all promoted tweets...')
-
-    // create an array of promoted tweets HTML
-    var array = []
-    var tweets = document.getElementsByClassName('promoted-tweet')
-
-    for (var i = 0; i < tweets.length; i++) {
-      var tweet = tweets[i].innerHTML
-      array.push(tweet)
-    }
-
-    return array
-  })
+  .evaluate(scrape)
   .end()
-  .then(function (results) {
-    // write each promoted tweet to text file
-    for (var i = 0; i < results.length; i++) {
-      jsdom.env(results[i], function (err, window) {
-        // extract username, url and tweet from HTML markup
+  .then(scan)
+  .catch(function (error) {
+    console.log('There was an error: ', error)
+  })
+
+// search DOM for promoted tweets
+function scrape () {
+  console.log('Scraping all promoted tweets...')
+
+  // push innerHTML of promoted tweets to array
+  var array = []
+  var tweets = document.getElementsByClassName('promoted-tweet')
+
+  for (var tweet of tweets) {
+    array.push(tweet.innerHTML)
+  }
+
+  return array
+}
+
+function scan (tweets) {
+  for (var tweet of tweets) {
+    jsdom.env(tweet, function (err, window) {
+      if (err) {
+        throw err
+      } else {
+        // extract username, url, text from HTML markup
         var username = window.document.getElementsByClassName('js-action-profile-name')[1].getElementsByTagName('b')[0].textContent
         var url = 'https://twitter.com' + window.document.getElementsByClassName('js-permalink')[0].href
-        var tweet = window.document.getElementsByClassName('js-tweet-text')[0].textContent
+        var text = window.document.getElementsByClassName('js-tweet-text')[0].textContent
 
-        // compose a timestamp (eg. 2016-12-25_09:00:05:175)
+        // compose timestamp (eg. 2016-12-25_09-00-05-175)
         var timestamp = dateFormat(new Date(), 'yyyy-mm-dd_HH-MM-ss-l')
 
         // compose filename
-        var filename = path.resolve(__dirname, timestamp + '_' + username)
+        var filename = path.resolve(__dirname, `${timestamp}_${username}`)
 
         // compose text file content
-        var output = 'Username: ' + username + '\n\n' +
-                     'URL: ' + url + '\n\n' +
-                     'Tweet: ' + tweet
+        var output = `Username: ${username}\n\nURL: ${url}\n\nTweet: ${text}`
 
-        // export tweet as a text file
-        fs.writeFile(filename + '.txt', output, function (err) {
-          if (err) {
-            throw err
-          } else {
-            // export tweet as an image
-            console.log('Wrote a text file!')
-            require('nightmare')({ show: false })
-              .goto(url)
-              .wait(1500)
-              .screenshot(filename + '.png')
-              .end()
-              .then(function () {
-                console.log('Took a screenshot!')
-              })
-              .catch(function (error) {
-                console.log('There was an error: ', error);
-              })
-          }
-        })
-      })
+        // write to disk
+        saveToText(filename, output)
+        saveToImage(filename, url)
+      }
+    })
+  }
+}
+
+// export tweet as text file
+function saveToText (filename, output) {
+  fs.writeFile(filename + '.txt', output, function (err) {
+    if (err) {
+      throw err
+    } else {
+      console.log('Wrote a text file!')
     }
   })
-  .catch(function (error) {
-    console.log('There was an error: ', error);
-  })
+}
+
+// export tweet as image
+function saveToImage (filename, url) {
+  require('nightmare')({ show: true })
+    .goto(url)
+    .wait(1500)
+    .screenshot(filename + '.png')
+    .end()
+    .then(function () {
+      console.log('Took a screenshot!')
+    })
+    .catch(function (error) {
+      console.log('There was an error: ', error)
+    })
+}
